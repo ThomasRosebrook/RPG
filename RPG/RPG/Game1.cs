497230f117ac1ef6a14ca43210a2ef256b0e1f31
lcmoeller <lcmoeller@ksu.edu>
2024-11-09 17:03:07 -0600

    hello thomas. gl man

--- RPG/RPG/Game1.cs
+++ RPG/RPG/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Threading;
 
 namespace RPG
 {
@@ -8,7 +9,7 @@ namespace RPG
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        private Tilemap tilemap;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,15 +19,18 @@ namespace RPG
 
         protected override void Initialize()
         {
+            _graphics.PreferredBackBufferWidth = 900;
+            _graphics.PreferredBackBufferHeight = 900;
+            _graphics.ApplyChanges();
             // TODO: Add your initialization logic here
-
+            tilemap = new Tilemap("Tilemap8.txt");
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            tilemap.LoadContent(Content);
             // TODO: use this.Content to load your game content here
         }
 
@@ -42,8 +46,10 @@ namespace RPG
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            GraphicsDevice.Clear(new Color(32, 18, 8));
+            _spriteBatch.Begin();
+            tilemap.Draw(gameTime, _spriteBatch);
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
