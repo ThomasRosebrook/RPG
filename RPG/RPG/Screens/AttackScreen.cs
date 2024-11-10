ba0ef4264dafaf91f9d4cff0f1b74c456ee91087
ThomasRosebrook <trosebr@ksu.edu>
2024-11-09 19:14:45 -0600

    Created Bottom Menu Class

---
+++ RPG/RPG/Screens/AttackScreen.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using RPG.ScreenManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Screens
+{
+    public class AttackScreen : BottomMenu
+    {
+        public AttackScreen() : base("Physical", "Magic", "Bribe", "Other")
+        {
+
+        }
+
+
+        protected override void SelectOptionOne()
+        {
+
+        }
+
+        protected override void SelectOptionTwo()
+        {
+
+        }
+
+        protected override void SelectOptionThree()
+        {
+
+        }
+
+        protected override void SelectOptionFour()
+        {
+
+        }
+    }
+}
